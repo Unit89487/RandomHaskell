03abd5b8a1ea2d66f46643e25d1da3ae5e2a58b5
Jay Abdallah <jay.h.abdallah@gmail.com>
2014-12-04 15:07:41 -0500

    Couple of functions

---
+++ functionfun.hs
@@ -0,0 +1,6 @@
+--Introduction to functions in Haskell, courtesy of Real World Haskell ebook
+
+add a b = a + b 
+cube a = a ^ 3
+squareRootOfNegativeUnity a b = a ++ "i"
+achillesSpeech a = print (a ++ " Boomed Achilles, bravely.") 
