44323bedd7b725cd91423343032136080a96ed3e
Jay Abdallah <jay.h.abdallah@gmail.com>
2014-12-15 19:46:47 -0500

    Couple of more functions.

--- functionfun.hs
+++ functionfun.hs
@@ -4,3 +4,28 @@ add a b = a + b
 cube a = a ^ 3
 squareRootOfNegativeUnity a b = a ++ "i"
 achillesSpeech a = print (a ++ " Boomed Achilles, bravely.") 
+
+-- dec 14, 2014
+
+whatev a b = if tail a > 5 
+			 then a ++ b 
+			 else b ++ a 
+
+
+
+
+listAdder a b = if a /= b 
+				   then a ++ b
+				   else b ++ a 
+
+-- dec 15, 2014
+
+blah a b = if tail [1 .. 10] >= tail [1 .. 8]
+		   then print "I really need to think of some more interesting functions."
+		   else print "Does it matter?"
+
+tada a b = if tail (a ++ b) < 10
+		   then 35:a 
+		   else 35:b
+
+cha ching a b = 
--- myDropFunc.hs
+++ myDropFunc.hs
@@ -3,3 +3,7 @@
 myDrop n string = if n <= 0 || null string 
 				  then string 
 				  else myDrop (n - 1) (tail string)
+
+educateYourself a b = if a > b || a == b || a < b
+					  then print "This couldn't possibly be wrong!"
+					  else print "In Haskell, the || operator short circuits."
