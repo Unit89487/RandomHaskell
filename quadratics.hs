e257ba40d0e1d69fe760f8bc440668a14ddca4cb
Jay Abdallah <jay.h.abdallah@gmail.com>
2014-12-22 22:31:29 -0500

    quadeartics

---
+++ quadratics.hs
@@ -0,0 +1,9 @@
+--What to do in here? 
+--ax^2 + bx + c = 0. 
+--What else? the equation 
+
+-- (-b +- (sqrt(b^2 - 4ac))) / 2ab 
+-- The quadratic function deals with the co-efficients of the quadratic only
+
+quadraticEquation a b c = if a /= 0 
+			  			  then -b + 
