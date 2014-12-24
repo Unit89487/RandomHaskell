f50c7fc719db5b201ca661e056fbe159b071bede
Jay Abdallah <jay.h.abdallah@gmail.com>
2014-12-23 21:30:37 -0500

    quadratic formula

--- quadratics.hs
+++ quadratics.hs
@@ -2,8 +2,9 @@
 --ax^2 + bx + c = 0. 
 --What else? the equation 
 
--- (-b +- (sqrt(b^2 - 4ac))) / 2ab 
+-- (-b +- (sqrt(b^2 - 4ac))) / 2a
 -- The quadratic function deals with the co-efficients of the quadratic only
 
-quadraticEquation a b c = if a /= 0 
-			  			  then -b + 
+quadraticFormula a b c = (-b + sqrt(b^2 - (4*a*c)))/2*a 
+
+
