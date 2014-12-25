19d675229a6433695bc25f9fd25aaa8bffe76cb4
Jay Abdallah <jay.h.abdallah@gmail.com>
2014-12-24 22:52:28 -0500

    No code. Lame. Just a verbal description of future Haskalculus functions.

---
+++ calculus.hs
@@ -0,0 +1,10 @@
+--First functions for Haskalculus. I'm going to try to create my own version of what is on this guy Ben Kovach's math and programming
+--blog. I haven't had my Calculus maths yet, but I'm pretty sure he's taking a derivative. d divided by the product of 
+--itself and x (the derivative?), of f of x, equals the limit as h approaches zero of f of the sum of x and h less f of x, divided by 
+--h.
+
+--How do we program that bad boy?
+
+--Ben approximates the derivative by substituting a small decimal number for h instead of calculating the limit of the function
+--as it goes to zero. 
+
