7c61c87d5355d22f5ba94463c9ade601b23fb640
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-04 00:07:03 -0500

    Practice with types from Chap 2. Didn't do much. Need better time management. Worked with some hardware today, dissasembled a Dell PC and went over the components, went to Micro Center to shop for components. Looked at some interesting Rasp Pis, Rasp Pi sensor kits, wireless chips, talked to a guy there about his plan to build a helicopter using the gyrscope and a GPS chip, pros and coins of using a full-blown wireless chip vs Bluetooth. Besides the minimal Haskell, a lot learned toady.

---
+++ Ch2PracticeOne.hs
@@ -0,0 +1,9 @@
+
+
+
+aTuple = (5, "Chimichangas")
+anotherTuple = (9, "Churros")
+yetAnotherTuple = (19000, "Huevos Rancheros")
+
+compareFirstTupleElement tupleOne tupleTwo = if fst tupleOne > fst tupleTwo then print "The first element in the first tuple is larger than the first element of the second tuple. Isn't that insanse?" else tupleOne 
+
