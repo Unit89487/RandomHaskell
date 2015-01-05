3beae831eaab0b7d9826a5a42bcefa314bad4d7d
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-04 22:02:04 -0500

    Ch2 practice

--- Ch2PracticeOne.hs
+++ Ch2PracticeOne.hs
@@ -1,9 +1,20 @@
 
-
+states = ["Virginia", "Kentucky", "New Jersey", "Chicago", "Illannoyed"]
+capitals = ["Boise", "Sioux Falls", "Indianapolis", "Austin"]
+neighborhoods = ["East Village", "Alphabet City", "Bowling Green", "Tribeca"]
 
 aTuple = (5, "Chimichangas")
 anotherTuple = (9, "Churros")
 yetAnotherTuple = (19000, "Huevos Rancheros")
 
-compareFirstTupleElement tupleOne tupleTwo = if fst tupleOne > fst tupleTwo then print "The first element in the first tuple is larger than the first element of the second tuple. Isn't that insanse?" else tupleOne 
+isTrue = True || False
+isFalse = True && False 
+
+
+
+
+--types to use: list, tuple, boolean, string, number, float, 
+--Prelude functions to try and use: compare, odd, head, tail, drop,    fst, snd 
+--logical operators to use  && && ||   == 
+--principles (logical principles? programming principles? mathematical principles?) recursion 
 
