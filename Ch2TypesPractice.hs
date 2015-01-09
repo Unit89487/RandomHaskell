6a23df3bcd4b79e1e729253adb90915866f3a5a1
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-09 10:18:10 -0500

    Types: bool, char, int, integer, double, float

---
+++ Ch2TypesPractice.hs
@@ -0,0 +1,37 @@
+--Going to write some code with all of the types in Haskell. 
+
+--Char
+
+someChar = 'a'
+anotherChar = 'b'
+moreChar = 'c'
+onParForChar = 'd'
+
+
+--Bool
+
+aBool = True && True && True
+anotherBool = True && False && True
+moreBool = False || False || False || False || True
+keepTheBoolComing = (False || True) && True && True 
+georgeBoole = (False || False || True) && False 
+kaBool = False && True && True && True && True && True && True 
+ 
+--List
+
+listOfChars = ['e','f','g','h','i']
+
+
+
+
+--Tuple 
+
+
+
+
+
+--Chat with Dr. Muchmore 
+---The most important factor in developing software is domain expertise. do you really understand the area you;re working in? 
+---you basically reproduce what u do 
+---the biggrest complement we've got is that someone pulls up the screen and says "this may not be exactly how I do my job, but I know about fifteen physicians who do"
+---
