44ab4f3b668218df9be5dee2329aae76d0a986b9
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-11 11:35:39 -0500

    Primitive types, lists and tuples

--- Ch2TypesPractice.hs
+++ Ch2TypesPractice.hs
@@ -20,22 +20,70 @@ kaBool = False && True && True && True && True && True && True
 
 --Int
 
+anInt = 50
+anotherInt = 8 
+yetAnotherInt = 25
+moreInt = 100
+thisInt = anInteger + anotherInter + yetAnotherInt
+
+
 --Integer
 
+anInteger = 50
+anotherInteger = 8
+yetAnotherInteger = 25
+moreInteger = 100
+thisInteger = anInteger + anotherInteger + yetAnotherInteger + moreInteger
+
+
+--Int or Integer?
+
+--Double or Int?
+
+doubleOrIntOne = 104.3 + 8
+doubleOrIntTwo = 3.5 + 4
+
 --Double
+
+aDouble = 4.5
+dubbs = 10.5
+anotherDouble = 12.4
+yetAnotherDouble = 101.2
+moreDoubles = 209.2
+lastDouble = aDouble + dubbs + anotherDouble + yetAnotherDouble + moreDoubles
+doubleDoubleSum = 129.3 + lastDouble
+
 
 --List
 
 listOfChars = ['e','f','g','h','i']
 listOfStrings = [['a','b','c'], ['d','e','f'], ['h','e','g','f'], ['s','o','m','e'], ['s','t','r','i','n','g'], ['h','e','r','e']]
-
-
-
+listOfStringsTwo = ["abc", "def","ghi","jklmnop"]
+listOfListsofStrings = [["list", "of","strings"], ["another", "list", "of", "strings"], ["more", "lists", "of", "type", "string"]]
+listOfBools = [True, False, False, True, False, True]
+listOfInts = [20, 39, 393, 3030, 30, 11, 23, 403, 303, 30, 1]
+listOfDoubles = [202.4, 4934.3, 40.4040. 4030.402, 403.30, 123.30, 0.0002, 203.0, 3030.023]
+listOfTuplesOfCharAndBool = [('a', True), ('b', False), ('c', False), ('d',True), ('e', False), ('f', True), ('g', False)]
 
 --Tuple 
+--Type + every other type. Char, Bool, Int, Integer, Double,  
+
+tupleOfCharAndChar = ('d','e')
+tupleOfCharAndBool = ('a', True)  --this is a two tuple, since we call tuples n-tuples, where n is the number of elemets in the tuple.
+tupleOfCharAndInt = ('c', 50)
+tupleOfCharAndDouble = ('a', 191.3)
+
+
+tupleOfDoubleAndChar = (29.3, 'c')
+tupleOfDoubleAndInt = (192.3, 18)
 
 
 
+
+--Jan 11. This is a general practice template. Produce variables using all of languages types. Produce collection types containing all of those types.
+--Next step (?) Produce statements using all of the language's operators. Produce statements using all of the language's keywords.
+--Next step (??) Produce statements using most commonly used library functions/methods. 
+--? indicates uncertainty. Haven't tried that stuff out. No idea if it's effective. 
 
 
 --Chat with Dr. Muchmore 
