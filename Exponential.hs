e02b008afde07a1d830e0ac7be9101cfa7fb90ea
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-12 20:46:15 -0500

    exponentiation with recursion

---
+++ Exponential.hs
@@ -0,0 +1,15 @@
+--Exponential function
+--What does the Haskell library already have?
+--It's already got an exponential operator, or "potentiation" as the old-school folks like to call it. 
+
+x = 5
+y = 3
+
+
+--potentiate is a times a, b times. 
+potentiate a b = if b > 0 then a * potentiate a (b - 1) else a 
+
+--Step 1 potentiate 2 3
+--2 *  potentiate 2 2
+--Step 2
+--2 * [2 * 2 * 1]
