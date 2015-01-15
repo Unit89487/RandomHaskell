f30bf842023cf5e2d8e15c610f717feacb2e5926
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-15 12:14:32 -0500

    Cube function

---
+++ Haskalculus/2015-15-1-Chap1.hs
@@ -0,0 +1,11 @@
+--Functions encountered in Chapter 1 of the text book "Calculus. Early Transcendentals. Second Edition" by Briggs, Cochran and Gillette
+
+-- f(x) is equivalent to function x 
+
+function y = y^2 + 1   --standard quadratic function of the form ax2 + bx + c where b = 0 in this case. 
+functionTwo t = sqrt(4 - (t^2)) 
+functionThree u = 1 / (u - 1)
+
+--Q: state the domain and range of each function.
+
+cubeItPlusOne gobbedlyGork = (gobbedlyGork * gobbedlyGork * gobbedlyGork) + 1 
